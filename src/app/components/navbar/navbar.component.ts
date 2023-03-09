f9b0f0d43a95f148ab5c74c83ab55acf2e8745d1
LeeDaeMin <laalmeida@uts.edu.co>
2023-03-09 10:46:56 -0500

    agregando la libreria awesome por cdk

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faBell, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-navbar',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  faBell = faBell;
+  faInfoCircle = faInfoCircle;
+
   isOpen = false;
+/*   isOpenBody = false; */
 
   constructor() { }
 
