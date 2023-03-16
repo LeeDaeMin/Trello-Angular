5500b757b61b933f0e0d2721b538573efd25d514
LeeDaeMin <laalmeida@uts.edu.co>
2023-03-16 12:01:25 -0500

    Agregando buenas practicas y mapeo de colores

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faBell, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+import { faBell, faInfoCircle, faClose } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-navbar',
@@ -8,6 +8,7 @@ import { faBell, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 export class NavbarComponent implements OnInit {
 
   faBell = faBell;
+  faClose = faClose;
   faInfoCircle = faInfoCircle;
 
   isOpen = false;
